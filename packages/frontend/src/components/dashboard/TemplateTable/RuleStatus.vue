99f637d6114c29c47ecf78fc63f95b8739eb7906
MasterSplinter <25282637+Splinter0@users.noreply.github.com>
2024-11-21 01:20:15 +0100

    Added filtering by status and fixed colors (#27)
    * Hacky solution to fix filtering
    
    * Update SDK and use HTTPQL api to filter reuqests
    
    * Add table filtering based on status
    
    * Add status colors back

--- packages/frontend/src/components/dashboard/TemplateTable/RuleStatus.vue
+++ packages/frontend/src/components/dashboard/TemplateTable/RuleStatus.vue
@@ -11,21 +11,21 @@ defineProps<{
   <Chip
     v-if="status === 'Unexpected'"
     v-tooltip="'This access control rules on this resource are not as expected'"
-    style="background: var(--p-yellow-500); padding: 0.25rem 0.5rem; font-size: 0.8rem; gap: 0.25rem">
+    class="status unexpected-status">
     <i class="fas fa-exclamation-triangle" />
     <span>Unexpected</span>
   </Chip>
   <Chip
     v-else-if="status === 'Enforced'"
     v-tooltip="'This access control rules on this resource are properly enforced'"
-    style="background: var(--p-green-500); padding: 0.25rem 0.5rem; font-size: 0.8rem; gap: 0.25rem">
+    class="status enforced-status">
     <i class="fas fa-check" />
     <span>Enforced</span>
   </Chip>
   <Chip
     v-else-if="status === 'Bypassed'"
     v-tooltip="'This access control rules on this resource have been bypassed'"
-    style="background: var(--p-red-500); padding: 0.25rem 0.5rem; font-size: 0.8rem; gap: 0.25rem">
+    class="status bypassed-status">
     <i class="fas fa-x" />
     <span>Bypassed</span>
   </Chip>
